4446a5eaba58f0263adffd2f8bd69c6d634a99c6
boploi <boploi198@gmail.com>
2021-03-31 11:09:10 +0700

    update

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,8 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-# import math
-# import random
+
 
 class AdminSiteTests(TestCase):
 
