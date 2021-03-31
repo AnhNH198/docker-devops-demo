04d6211704d1ec3d93409c11851afda60732f114
boploi <boploi198@gmail.com>
2021-03-31 10:33:35 +0700

    update

--- app/core/models.py
+++ app/core/models.py
@@ -1,43 +1,32 @@
-import uuid
-import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
-from django.conf import settings
-
-
-def recipe_image_file_path(instance, filename):
-    """Generate file path for new recipe image"""
-    ext = filename.split('.')[-1]
-    filename = f'{uuid.uuid4()}.{ext}'
-
-    return os.path.join('uploads/recipe/', filename)
+     PermissionsMixin
 
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new user"""
+        """default password set to None
+        add extra_field in case you need add more argument
+        -> make function more flexible"""
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError('User must specify Email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
+        # using=self._db is required for support multiple databases
         return user
 
     def create_superuser(self, email, password):
-        """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that suppors using email instead of username"""
+    """Custom User model that support email login instead of Username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -46,45 +35,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
-
-class Tag(models.Model):
-    """Tag to be used for a recipe"""
-    name = models.CharField(max_length=255)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
-
-    def __str__(self):
-        return self.name
-
-
-class Ingredient(models.Model):
-    """Ingredient to be used in a recipe"""
-    name = models.CharField(max_length=255)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-
-    def __str__(self):
-        return self.name
-
-
-class Recipe(models.Model):
-    """Recipe object"""
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-    title = models.CharField(max_length=255)
-    time_minutes = models.IntegerField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    link = models.CharField(max_length=255, blank=True)
-    ingredients = models.ManyToManyField('Ingredient')
-    tags = models.ManyToManyField('Tag')
-    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
-
-    def __str__(self):
-        return self.title
